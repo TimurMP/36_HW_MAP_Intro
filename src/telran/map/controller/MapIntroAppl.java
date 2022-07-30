b9e3fe781f6903a5fa277d8f218b99e321ffc127
TimurMP <titimurmamyshev@gmail.commur>
2022-07-30 12:07:49 +0300

    Final 1.0

--- src/telran/map/controller/MapIntroAppl.java
+++ src/telran/map/controller/MapIntroAppl.java
@@ -1,2 +1,56 @@
-package telran.map.controller;public class MapIntroAppl {
+package telran.map.controller;
+
+import java.util.*;
+
+public class MapIntroAppl {
+    public static void main(String[] args) {
+        Map<String, Integer> usa = new TreeMap<>();
+        usa.put("Denver", 600_000);
+        usa.put("Boston", 670_000);
+        usa.put("Chicago", 2_700_000);
+        usa.put("Atlanta", 470_000);
+        usa.put("NY", 8_500_000);
+        usa.put("Dallas", 1_300_000);
+
+        Integer num = usa.get("Chicago");
+        System.out.println(num);
+        num = usa.get("Detroit");
+        System.out.println(num);
+        System.out.println(usa.size());
+        System.out.println(usa.isEmpty());
+        System.out.println(usa.containsKey("Boston"));
+        System.out.println(usa.containsKey("Bostondddd"));
+        System.out.println(usa.containsValue(600000));
+        System.out.println(usa.containsValue(700000));
+        num = usa.put("Chicago", 2_700_001);
+        System.out.println(num);
+        num = usa.get("Chicago");
+        System.out.println(num);
+
+        Collection<Integer> values = usa.values();
+        int total = 0;
+        for (Integer value : values) {
+            total+= value;
+        }
+        System.out.println("Total: " + total);
+
+        System.out.println("========Method 1==========");
+        Set<String> keys = usa.keySet();
+        for (String key : keys) {
+            System.out.println(key + " -> " + usa.get(key));
+
+        }
+
+        System.out.println("========Method 2==========");
+        Set<Map.Entry<String, Integer>> entries = usa.entrySet();
+        for (Map.Entry<String, Integer> entry : entries) {
+            System.out.println(entry);
+            System.out.println(entry.getKey() + "->" + entry.getValue());
+
+        }
+
+
+
+
+    }
 }
--- src/telran/map/controller/WordsFreqAppl.java
+++ src/telran/map/controller/WordsFreqAppl.java
@@ -1,2 +1,69 @@
-package telran.map.controller;public class WordsFrewAppl {
+package telran.map.controller;
+
+import java.util.*;
+
+public class WordsFreqAppl {
+    public static void main(String[] args) {
+        String[] words = {"kkk", "kkk", "kkk", "kkk", "kkk", "abc", "ab", "aaa", "ab", "aaa", "a", "aaa", "a", "a", "a"};
+
+        printWordsFrequency(words);
+    }
+
+    private static void printWordsFrequency(String... words) {
+        Map<String, Integer> res = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+//            if (res.containsKey(words[i])) {
+//                res.put(words[i], res.get(words[i]) + 1);
+//            } else {
+//                res.put(words[i], 1);
+//            }
+//
+//            if (res.putIfAbsent(words[i] , 1)!= null){
+//                res.put(words[i], res.get(words[i]) + 1);
+//            }
+//
+//            res.computeIfPresent(words[i], (k, v) -> v +1);
+//            res.computeIfAbsent(words[i], k -> 1);
+
+            res.merge(words[i], 1, (oldV, v) -> oldV + v);
+        }
+        System.out.println("Unsorted: ==========");
+        res.keySet().forEach(k -> System.out.println(k + " ---> " + res.get(k)));
+
+
+        System.out.println("Sorted: ==========");
+        Comparator<Map.Entry<String, Integer>> comp = new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        };
+        List<Map.Entry<String, Integer>> arrL = new ArrayList<>(res.entrySet());
+        Collections.sort(arrL, comp);
+
+
+        arrL.forEach(e -> System.out.println(e.getKey() + " ---> " + e.getValue()));
+
+//        Map<String,Integer> tempTreeMap = new TreeMap<String,Integer>()
+//        for (Map.Entry<String, Integer> stringIntegerEntry : arrL) {
+//            tempTreeMap.put(stringIntegerEntry.getKey(), stringIntegerEntry.getValue());
+//
+//        }
+//        tempTreeMap.keySet().forEach(k -> System.out.println(k + " ---> " + tempTreeMap.get(k)));
+//
+//        System.out.println(arrL);
+//
+//        }
+
+
+    }
 }
+
+
+
+
+
+
+
+
